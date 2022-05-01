2275e77c2614be516ce9c37eb3dbd666574d4161
Rounok Das <52637799+Rounok123@users.noreply.github.com>
2022-05-02 00:59:25 +0530

    6th Commit

--- Request.py
+++ Request.py
@@ -1,9 +1,9 @@
 from pydantic import BaseModel
 # 2. Class which describes Bank Notes measurements
 class weatherData(BaseModel):
-    a : float 
-    b : float 
-    c : float 
-    d : float
-    e : float
-    f : float
+    Temperature : float 
+    Apparent_Temp : float 
+    Wind_Speed : float 
+    Wind_Bearing : float
+    Visibility : float
+    Pressure : float
--- Weather.py
+++ Weather.py
@@ -3,37 +3,39 @@ import uvicorn
 from fastapi import FastAPI
 from Request import weatherData
 import pickle
-# 2. Create the app object
+
+# 2. Create the app object and load the pickel file
 app = FastAPI()
 pickle_in = open("model.pkl","rb")
 model = pickle.load(pickle_in)
 
-# Expose the prediction functionality, make a prediction from the passed
-#    JSON data and return the predicted Bank Note with the confidence
+# 3. Expose the prediction functionality, make a prediction from the passed
+# JSON data and return the predicted Humidity Percentage
 @app.get('/')
 def Home():
     return{
-        '/Predict should be api endpoit for predicting Humidity'
+        '/Predict should be the api endpoint for predicting Humidity'
     }
 @app.post('/predict')
 def predict_weather(data:weatherData):
     dataset = data.dict()
-    A =dataset['a']
-    B =dataset['b']
-    C =dataset['c']
-    D =dataset['d']
-    E =dataset['e']
-    F =dataset['f']
+    A =dataset['Temperature']
+    B =dataset['Apparent_Temp']
+    C =dataset['Wind_Speed']
+    D =dataset['Wind_Bearing']
+    E =dataset['Visibility']
+    F =dataset['Pressure']
 
     Test_data = [[A,B,C,D,E,F]]
     Predicted_Value = model.predict(Test_data)
     Predict = Predicted_Value[0]
+    Format_Predict = "{:.2f}".format(Predict)
+    Format_Predict = float(Format_Predict)*100
     return {
-        'prediction': {'Humidity':Predict}
+        'prediction': {'Humidity (%)':Format_Predict}
     }
 
-# 5. Run the API with uvicorn
-#    Will run on http://127.0.0.1:8000
+# 4. Run the API with uvicorn
 if __name__ == '__main__':
     uvicorn.run(app, host='127.0.0.1', port=8000)
     
